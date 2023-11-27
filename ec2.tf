b961d1e1b23fc052b113da7120e49a0991314814
zibago <56084675+zibago@users.noreply.github.com>
2023-11-27 00:31:00 +0000

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,10 @@
 resource "aws_instance" "app_server" {
   ami           = "ami-0ad9f4c7544ed8cea"
   instance_type = "t2.micro"
+  subnet_id = "subnet-0c7410cb7c654ea03"
 
   tags = {
-    Name = "ProdInstance2"
+    Name = "devInstance2"
   }
 }
 #test
